7bce35fac5402a494a3eb11b4b2b652b8f179850
Manuel Lara <manuellaraa@gmail.com>
2017-02-27 15:43:19 -0800

    change

--- HW1/HW1/HW1.py
+++ HW1/HW1/HW1.py
@@ -6,9 +6,13 @@
 
 
 name = input("What is your name? ")
-age = int(input("\nwhat is your age? "))
+age = int(input("\nWhat is your age? "))
+randNum = int(input("\nEnter random number: "))
 
 solution = 2017 + (100 - age)
 
-print ("\nYour name is ", name, " and you are ", age, " years old.")
-print("\nYou will turn 100 in the year ", solution, ".\n")
+count = 0
+while count < randNum:
+    print ("\nYour name is ", name, " and you are ", age, " years old.")
+    print("You will turn 100 in the year ", solution, ".\n")
+    count +=1
