187d32e971f1b85d0202bb2cd32a1c20272bfc66
Manuel Lara <manuellaraa@gmail.com>
2017-02-27 15:34:03 -0800

    hw1

---
+++ HW1/HW1/HW1.py
@@ -0,0 +1,14 @@
+#############################
+# homework 1
+# Manuel Lara
+#############################
+
+
+
+name = input("What is your name? ")
+age = int(input("\nwhat is your age? "))
+
+solution = 2017 + (100 - age)
+
+print ("\nYour name is ", name, " and you are ", age, " years old.")
+print("\nYou will turn 100 in the year ", solution, ".\n")
