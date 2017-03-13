6315a10de61f5834db723329f158500249344111
Manuel Lara <manuellaraa@gmail.com>
2017-03-13 15:41:53 -0700

    correction

--- HW3/HW3/HW3.py
+++ HW3/HW3/HW3.py
@@ -3,14 +3,13 @@
 # Manuel Lara
 #########################
 
-
 apples = .65
 oranges = .90
 bananas = .70
 
-x = int( input("How many Apples would you like to buy? ") )
-y = int( input("How many Oranges would you like to buy? ") )
-z = int( input("How many Bananas would you like to buy? ") )
+x = float( input("How many Apples would you like to buy? ") )
+y = float( input("How many Oranges would you like to buy? ") )
+z = float( input("How many Bananas would you like to buy? ") )
 
 totalApples = x * apples
 totalOranges = y * oranges
@@ -22,8 +21,8 @@ total = totalApples + totalBananas + totalOranges
 print("\n\n\n\n\n\n")
 print ("BOB'S FRUIT GROCERY INVOICE")
 print ("-" * 60 )
-print("     ",x , " Apples @ .65 each = ", round(totalApples, 2) )
-print("     ",y , " Oranges @ .90 each = ", round(totalOranges, 2) )
-print("     ",z , " Bananas @ .70 each = ", round(totalBananas, 2) )
+print("     ", x , " Apples @ .65 each = %8.2f" % (totalApples) )
+print("     ", y , " Oranges @ .90 each = %8.2f" % (totalOranges) )
+print("     ", z , " Bananas @ .70 each = %8.2f" % (totalBananas) )
 print(" " * 20, "-" * 20 )
-print(" " * 30, "$", round(total, 2) )
+print(" " * 25, "$ %8.2f" % (total) )
