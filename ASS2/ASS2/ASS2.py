d6ac8002e90ef1116b04326361e13479f4b3c769
Manuel Lara <manuellaraa@gmail.com>
2017-04-03 17:09:40 -0700

    final

--- ASS2/ASS2/ASS2.py
+++ ASS2/ASS2/ASS2.py
@@ -6,16 +6,66 @@
 #
 # Algorithm 
 # 
-# STEP 1:
+# 
+# STEP 1: INPUT
+# prompt user for name
+# prompt user if they would like to create an ammortization table with either 'y' or 'n'
+# error checking :: if 'n', the program will exit. if not 'y' or 'n', the program will exit. if 'y', the program proceeds 
+# prompt user to enter loan amount
+# error checking :: if input is less than 0 or not a number, the program will exit. if it is, the program proceeds 
+# prompt user for interest rate
+# error checking :: if input is less than 0 or not a number, the program will exit. if it is, the program proceeds to divide interest rate by 100 to convert to decimal 
+# prompt user to enter amount of years they want to pay off loan
+# error checking :: if input is less than 0 or not a number, the program will exit.
 # 
 # 
+# STEP 2: PROCESS
+# define the first function and pass the name parameter to it
+# include a print statement with a discription + name
+# define the next function and pass the loan amount parameter to it
+# include a print statement with a discription + loan amount
+# define the third function and pass interest rate and number of years to pay off loan
+# set a variable to to find total amount of months ( years * 12 )
+# set another variable to find monthly interest rate ( interest rate / total months )
+# include print statement with a discription + monthly interest rate 
+# define fourth function and pass number of years to it
+# set a variable to get total months ( years * 12 )
+# include print statement with discription + total months 
+# define fifth function and pass loan amount , interest rate , years to pay loan
+# set a variable to get total loan amount after interest ( loan + ( loan * interest ) )
+# set a variable to get total amount of months ( years * 12 )
+# set variable to get monthly interest rate ( interest rate / total months )
+# set a variable to get part of the formula ( monthly rate + 1 ) ^ total months
+# set variable to get monthly payment ( loan amount * (montly rate * previus variable) / (previous variable - 1) )
+# include print statements with discription + monthly payments 
+# include print statements with discription + total cost of loan
+# define sixth function and pass loan amount , interest rate , years to pay loan
+# set variable to get total amount of months ( years * 12 )
+# set a variable to the loan amount
+# include a heading print statement that includes payment , beginning , monthly payment , interest , principal , ending balance
+# start a for loop from 1 to the amount of months + 1 
+# set variable to get total loan amount after interest rate  ( loan amount + (loan amount * interest rate) )
+# set variable to get total months ( years / 12 )
+# set variable to get monthly interest rate ( interest rate / total months )
+# set a variable to get part of the formula ( monthly rate + 1 ) ^ total months
+# set variable to get monthly payment ( loan amount * (montly rate * previus variable) / (previous variable - 1) )
+# set variable to monthly payment 
+# set variable to loan amount * monthly interest rate
+# set variable to monthly payment - interest 
+# set variable to loan amount - principal 
+# include print statement to print counter ( payment # ) , beginning , monthly payment , interest , principal , ending balance 
+# set a reset variable to set end balance to beginning   
 # 
 # 
-# STEP 2:
-# 
-# 
-# 
-# 
+# STEP 3 : OUTPUT
+# call first function and pass name parameter 
+# call second function and pass loan amount paramter 
+# call third function and pass interest rate , number of years 
+# call fourth function and pass number of years 
+# call fifth function and pass loan amount , interest rate , number of years 
+# call sixth function and pass loan amount , interest rate , and number of years 
+#
+#
 ###################################
 
 def intro( name ) :
@@ -37,12 +87,12 @@ def n( numOfYears ) :
     print ("Amount of payments: {:>50,.0f}" .format(n) )
     return 
 
-def A( p , interestRate , numOfYears ) :                                            #calculates payment amount 
+def A( p , interestRate , numOfYears ) :                                             
     t = p + (p * interestRate)
     n = numOfYears * 12
     r = interestRate / n 
     a = ( r + 1 )**n
-    mp = (p * ((r * a) /  (a - 1)))
+    mp = (p * ((r * a) /  (a - 1)))                                             #calculates payment amount
     print ("Monthly payment amounts: {:>45,.2f}" .format(mp) )
     print ("Total cost of loan: {:>50,.2f}\n\n" .format(t) )                         
     return 
@@ -66,7 +116,6 @@ def table( p, interestRate , numOfYears ) :
         beginning = endBalance
 
     return
-
 
 
 
