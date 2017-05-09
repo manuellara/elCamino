f05b0347a6dcab76aefc226f506d716c4e022faa
Manuel Lara <manuellaraa@gmail.com>
2017-05-09 13:59:17 -0700

    complete

--- ASS4/ASS4.py
+++ ASS4/ASS4.py
@@ -1,3 +1,95 @@
+#####################################################################
+# Manuel Lara
+# CS 14 - Assignment 4
+#
+# Algorithm 
+#
+# FUNCTION DEFINITIONS
+#
+# inputFile :
+# takes 2 parameters : the filename and the output name
+# open the fileName in 'read' mode
+# open the output in 'write' mode
+# reroute the print function to the output file
+# initialize count , totalNet , avgCounter , highestNet , lowestNet to None , lowestName
+# initialize a 'for' loop to go through every line in the file
+#   if the counter remainder is 0 only pick up the first line and CALL 'getName' and pass the line 
+#   else CALL 'getIDSalary' and pass the line - set the return values to ID , salary , avgCount
+#   CALL 'calculateTaxNet' and pass salary - set return values to tax , net
+#   CALL 'output' and pass count , name , ID , salary, tax , net
+#   append name to nameList 
+#   append ID to idList
+#   append salary to salList
+#   append tax to taxList
+#   append net to netList
+#   increment avgCounter with avgCount
+#   if agvCount does not equal 0 , add net to totalNet
+#   if count does not equal 0 , divide totalNet by avgCounter to get avgNet
+#   if net is greater that highestNet , set net to highestNet
+#   if  lowestNet is None , set lowestNet to net
+#   if net is less than lowest net and not negative , set net to lowestNet 
+#   increment count by 1
+# CALL 'findAverageHighestLowestNet' and pass avgNet , highestNet , highestName , lowestNet , lowestName
+# close input file
+# close output file
+#
+# sorting :
+# takes 5 parameters : nameList , idList , salList , taxList , netList 
+# zips lists together and stores result in data
+# then sort data by the '0' element ( names )
+# start a for loop from 0 to length of data
+# split up each of data's index to name , id , salary , tax , net
+# if ID length does not equal 4
+#   print invalid statement
+# if salary is less than 0
+#   print invalid statement 
+# if ID length is 4 and salary is greater than 0
+#   print statement 
+#
+# getName :
+# takes line as parameter 
+# strips the right space and sets the result to name
+# returns name
+#
+# getIDSalary : 
+# takes line as parameter and splits it  
+# stores variables in ID , salary 
+# if ID's length is 4 and salary is grater than 0
+#   set avgCount to 1
+#   else  set avgCount to 0
+# return ID , salary , avgCount 
+# 
+# calculateTaxNet :
+# takes salary as a parameter 
+# if salary <= 3500 : flatTax is 0 and tax  is 0%
+# if 3500 < salary <= 8000 : flatTax is 0 and tax is 6%
+# if 8000 < salary <= 20000 : flatTax is 270 and tax is 11% + 6% of 4500
+# if 20000 < salary <= 34000 : flatTax is 1590 and tax 17% + 6% of 4500 + 11% of 12000
+# if 34000 < salary <= 54000 : flatTax is 3970 and tax is 24% + 6% of 4500 + 11% of 12000 + 17% of 14000
+# if 54000 < salary : flatTax is 8770 and tax is 32%+ 6% of 4500 + 11% of 12000 + 17% of 14000 + 24% of 20000
+#
+# findAverageHighestLowestNet :
+# takes avgNet , highestNet , highestName , lowestNet , lowestName as parameters 
+# avg net income statement is printed 
+# highestNet income statement is printed 
+# lowestNet income statement is printed 
+#
+# output :
+# takes count , name , ID , salary , tax , net as parameters 
+# if count is 1 , print the table header 
+# if ID length is not 4 , print invalid statement 
+# if salary is less than 0 , print invalid statement 
+# if ID length is 4 and salary is greater than 0, print statement 
+#
+#
+# MAIN
+#
+# prompt user to enter file path and store it in fileName
+# prompt user to enter output file name and store it in outputName
+# CALL 'inputFile' and pass fileName and outputFile
+################################################################################
+
+
 # FUNCTIONS #
 def inputFile( fileName , outputName ) :
     inputFile = open(fileName , 'r')                                #open input file
@@ -157,10 +249,8 @@ def findAverageHighestLowestNet( avgNet , highestNet , highestName , lowestNet ,
 
 import sys
 # MAIN #
-fileName = input( "Drag a file in: " )                                      #comment out to automate - step 1
-outputName = input("Enter output file name :" )
-
-#fileName = "incomes.txt"                                                   #uncomment to automate - step 2
+fileName = input( "Drag a file in: " )                                     #gets file path
+outputName = input("Enter output file name :" )                            #gets name for output file
 
 # FUNTION CALL #
-inputFile( fileName , outputName )                                         
+inputFile( fileName , outputName )                                         #calls inputFile and passes 'fileName' and 'outputName'
